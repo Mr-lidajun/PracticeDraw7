1432d37779e2ac1c9caecc03f2c7971c20e1affd
lidajun828 <!QAZ2wsx123456>
2018-02-01 10:03:46 +0800

    HenCoder 绘制 7 属性动画 练习项目

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -49,10 +49,19 @@ public class Practice03OfObjectLayout extends RelativeLayout {
 
     private class PointFEvaluator implements TypeEvaluator<PointF> {
 
+        private PointF mPointF;
+
         // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+            float x = startValue.x + (fraction * (endValue.x - startValue.x));
+            float y = startValue.y + (fraction * (endValue.y - startValue.y));
+            if (mPointF == null) {
+                mPointF = new PointF(x, y);
+            } else {
+                mPointF.set(x, y);
+            }
+            return mPointF;
         }
     }
 }
